4ba1f6219d715bf16cfdfe6e214dfa6bd9b7003b
Lei Hua <leihua2009@gmail.com>
2016-10-04 21:30:29 -0400

    added greeting component in home sub folder

--- 01-modularization/src/app/app.module.ts
+++ 01-modularization/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HomeModule
   ],
   declarations: [
     AppComponent
