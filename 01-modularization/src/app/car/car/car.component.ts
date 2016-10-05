2a91b8948207f9f0e72e82a0439e7b0aa09d69d9
Lei Hua <leihua2009@gmail.com>
2016-10-04 21:49:43 -0400

    added car class

---
+++ 01-modularization/src/app/car/car/car.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'my-car',
+    templateUrl: './car.component.html'
+})
+export class CarComponent implements OnInit{
+    constructor() {}
+    ngOnInit(): void {}
+}
